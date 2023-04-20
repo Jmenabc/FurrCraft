143197790c90c4a5caba3539b61b2ad250b90422
Jmenabc <menajavier2002@gmail.com>
2023-04-20 23:36:29 +0200

    añadido lingote furro plus

--- Documents/forge-developer/src/main/java/com/jmena/furr/item/ModItems.java
+++ Documents/forge-developer/src/main/java/com/jmena/furr/item/ModItems.java
@@ -13,9 +13,12 @@ import net.minecraftforge.registries.RegistryObject;
     @author Jmenabc
  */
 public class ModItems {
-    //FURRO LINGOTE
+
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, "furrcraft");
+    //FURRO LINGOTE
     public static final RegistryObject<Item> furr_ingot = ITEMS.register("furr_ingot",()-> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    //FURRO LINGOTE PLUS
+    public static final RegistryObject<Item> furr_ingot_plus = ITEMS.register("furr_ingot_plus",()-> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
     //ESPADA FURRO
     public static final RegistryObject<Item> furr_word = ITEMS.register("furr_sword",
             ()-> new SwordItem(ModTiers.FURR,2,3f,new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
--- Documents/forge-developer/src/main/java/com/jmena/furr/item/ModTiers.java
+++ Documents/forge-developer/src/main/java/com/jmena/furr/item/ModTiers.java
@@ -5,5 +5,6 @@ import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraftforge.common.ForgeTier;
 
 public class ModTiers {
-    public static final ForgeTier FURR = new ForgeTier(2,1500,1.5f,2f,22, BlockTags.NEEDS_IRON_TOOL,()-> Ingredient.of(ModItems.furr_ingot.get()));
+    public static final ForgeTier FURR = new ForgeTier(2,1500,1.5f,2f,12, BlockTags.NEEDS_IRON_TOOL,()-> Ingredient.of(ModItems.furr_ingot.get()));
+    public static final ForgeTier FURRPLUS = new ForgeTier(10,2500,3f,6f,8, BlockTags.NEEDS_IRON_TOOL,()-> Ingredient.of(ModItems.furr_ingot_plus.get()));
 }
